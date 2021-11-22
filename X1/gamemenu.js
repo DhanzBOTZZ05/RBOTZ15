ee96a81435d8cdbb778196273b24255feecac9c3
DhanzBOTZZ05 <94766587+DhanzBOTZZ05@users.noreply.github.com>
2021-11-22 16:21:10 +0700

    Update gamemenu.js

--- X1/gamemenu.js
+++ X1/gamemenu.js
@@ -1,7 +1,7 @@
 const gamemenu = (tz, nama, namabot, prefix, day, yy, thisDay, bulan, wib, wita, wit) => { 
 	return `❲ *${namabot}* ❳
-${tz} *OWNER* : ${nama}
-${tz} *BOTNAME* : ${namabot}
+${tz} *OWNER* : ${Dhanz}
+${tz} *BOTNAME* : ${DHANZBOTZ}
 ${tz} *PREFIX* : *${prefix}* 
 ${tz} *TGGAL* : ${day}
 ${tz} *HARI KE* : ${yy}
@@ -17,15 +17,15 @@ ${tz} *${prefix}tebakkalimat*
 ${tz} *${prefix}tebakkata*
 ${tz} *${prefix}tebakkimia*
 ${tz} *${prefix}tebaklirik*
-${tz} *${prefix}tebaktebakan*
+
 ${tz} *${prefix}asahotak*
 ${tz} *${prefix}caklontong*
 ${tz} *${prefix}siapaaku*
 
 ❲ *INFO LAIN* ❳
-${tz} Jika bot tidak merespon, mungkin bot sedang off
+${tz} Jika bot tidak merespon, mungkin bot COLI
 ${tz} bot tidak menyimpan riwayat foto/media
-${tz} Silahkan beri waktu 5 detik penggunaan per fitur agar tidak menyebabkan spam
+${tz} Silahkan beri waktu 5 detik penggunaan per fitur agar tidak menyebabkan spam kontol
 ${tz} Jika menemukan bug/err silahkan hubungi owner
 `
 }
